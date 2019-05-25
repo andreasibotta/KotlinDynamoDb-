a3b4297c007f074eec8194ca0b98c11306fd3072
Andreas Lidholm <andreas.lidholm@ibotta.com>
2019-05-25 09:54:21 -0600

    Break out Table creator to own class

--- src/main/kotlin/test/andreas/kotlindynamodb/Main.kt
+++ src/main/kotlin/test/andreas/kotlindynamodb/Main.kt
@@ -1,53 +1,5 @@
 package test.andreas.kotlindynamodb
 
-import java.util.Arrays
-import com.amazonaws.client.builder.AwsClientBuilder
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder
-import com.amazonaws.services.dynamodbv2.document.DynamoDB
-import com.amazonaws.services.dynamodbv2.model.AttributeDefinition
-import com.amazonaws.services.dynamodbv2.model.KeySchemaElement
-import com.amazonaws.services.dynamodbv2.model.KeyType
-import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput
-import com.amazonaws.services.dynamodbv2.model.ScalarAttributeType
-
 fun main(args: Array<String>) {
-
-    val client = AmazonDynamoDBClientBuilder.standard()
-        .withEndpointConfiguration(AwsClientBuilder.EndpointConfiguration("http://localhost:4569", "us-west-2"))
-        .build()
-
-    val dynamoDB = DynamoDB(client)
-
-    val tableName = "Movies"
-
-
-    val exists = dynamoDB.listTables().any {
-        it.tableName == tableName
-    }
-    println(exists)
-
-    if (!exists) {
-        try {
-            println("Attempting to create table; please wait...")
-            val table = dynamoDB.createTable(
-                tableName,
-                Arrays.asList(
-                    KeySchemaElement("year", KeyType.HASH), // Partition
-                    // key
-                    KeySchemaElement("title", KeyType.RANGE)
-                ), // Sort key
-                Arrays.asList(
-                    AttributeDefinition("year", ScalarAttributeType.N),
-                    AttributeDefinition("title", ScalarAttributeType.S)
-                ),
-                ProvisionedThroughput(10L, 10L)
-            )
-            table.waitForActive()
-            println("Success.  Table status: " + table.description.tableStatus)
-
-        } catch (e: Exception) {
-            System.err.println("Unable to create table: ")
-            System.err.println(e.message)
-        }
-    }
+    TableCreator("Movies").create()
 }
---
+++ src/main/kotlin/test/andreas/kotlindynamodb/TableCreator.kt
@@ -0,0 +1,48 @@
+package test.andreas.kotlindynamodb
+
+import com.amazonaws.client.builder.AwsClientBuilder
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder
+import com.amazonaws.services.dynamodbv2.document.DynamoDB
+import com.amazonaws.services.dynamodbv2.model.*
+import java.util.*
+
+class TableCreator(private val tableName: String) {
+    fun create() {
+        val client = AmazonDynamoDBClientBuilder.standard()
+            .withEndpointConfiguration(AwsClientBuilder.EndpointConfiguration("http://localhost:4569", "us-west-2"))
+            .build()
+
+        val dynamoDB = DynamoDB(client)
+
+        val exists = dynamoDB.listTables().any {
+            it.tableName == tableName
+        }
+
+        if (exists) {
+            println("Table already exists")
+        } else {
+            try {
+                println("Attempting to create table; please wait...")
+                val table = dynamoDB.createTable(
+                    tableName,
+                    Arrays.asList(
+                        KeySchemaElement("year", KeyType.HASH), // Partition
+                        // key
+                        KeySchemaElement("title", KeyType.RANGE)
+                    ), // Sort key
+                    Arrays.asList(
+                        AttributeDefinition("year", ScalarAttributeType.N),
+                        AttributeDefinition("title", ScalarAttributeType.S)
+                    ),
+                    ProvisionedThroughput(10L, 10L)
+                )
+                table.waitForActive()
+                println("Success.  Table status: " + table.description.tableStatus)
+
+            } catch (e: Exception) {
+                System.err.println("Unable to create table: ")
+                System.err.println(e.message)
+            }
+        }
+    }
+}
